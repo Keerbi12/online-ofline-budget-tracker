b794610e253f50c322f96b7551a3892fb1533c00
Keerbi Liu <kirby.liu12@gmail.com>
2021-07-13 22:06:07 +1000

    Added event listener fetch for dynamic cache.

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,21 @@
-// Static assets
-const CACHE_NAME = "static-cache";
-const DATA_CACHE = "data-cache-v1";
+// caches
+const CACHE_NAME = "static-cache-v1";
+const DYNAMIC_CASHE = "dynamic-cashe-v1";
+
+// Array to cache
 const STATIC_CACHE = [
     "/",
     "/index.html",
-    "/css/style.css",
+    "/css/styles.css",
     "/js/app.js",
     "/js/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
 ];
 
-// Added an event listner for when the service worker is installed, will console log 
 // Added caches inside the install event handler, will only cache static assets since they will not change
-// Use evt.waitUntil to allow the promise to resolve, before the service worker is considered installed
 self.addEventListener("install", function(evt) {
+// Use evt.waitUntil to allow the promise to resolve, before the service worker is installed
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log("Cache successfull");
@@ -24,20 +25,50 @@ self.addEventListener("install", function(evt) {
     self.skipWaiting();
 });
 
-// Activating service worker, console log
-self.addEventListener("activate", evt => {
-    console.log("Service worker has been activated");
+// Deleting previous cache version, this allows updating of the service worker since a change is made. 
+// Activating service worker
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+// Names of the caches we have; static-cache-vn,
+        caches.keys().then(function(keys) {
+// Finds all keys(cache names), .filter checks to see if they are equal to CACHE_NAME (static-cache-vn)
+// If they are not equal, it will map it to an array, all keys(cache names) in that array will be deleted
+// when the CACHE_NAME gets changed, e.g. static-cache-vn+1, it will delete static-cache-vn, ensuring that the install will be retriggered due to the change.
+            return Promise.all(keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 });
 
-// Adding fetch event for requests
-self.addEventListener("fetch", evt => {
-   // console.log("fetch event requested", evt);
-//    evt.respondWith(
-//        caches.open(DATA_CACHE).then(catch => {
-//            return fetch(evt.request)
-//             .then(response => {
-//                 if (response.status === 2000)
-//             })
-//        })
-//    )
-});
+self.addEventListener('fetch', function(evt) {
+// When there is a fetch request, checks to see if the requested assets exist within the cache
+// will try to return the cached assets or || return the server assets
+    if (evt.request.url.includes("/api/transaction")) {
+        evt.respondWith(
+            caches.open(CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+
+
+    evt.respondWith(
+      caches.match(evt.request).then(cacheResponse => {
+        return cacheResponse || fetch(evt.request);
+      })
+    )
+  });
+  
+
