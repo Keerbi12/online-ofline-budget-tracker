1d96ec4d7368d5aef16367d7f3e79f0e678f5399
Keerbi Liu <kirby.liu12@gmail.com>
2021-07-13 17:51:24 +1000

    Added code in sw.js to listen for fetch events and console.log them on the console.

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
-console.log("his");
+// Added an event listner for when the service worker is installed, will console log 
+self.addEventListener('install', evt => {
+    console.log('Service worker has been installed');
+});
+
+// Activating service worker, console log
+self.addEventListener('activate', evt => {
+    console.log('Service worker has been activated');
+});
+
+// Adding fetch event for requests
+self.addEventListener('fetch', evt => {
+    console.log('fetch event requested', evt);
+});
